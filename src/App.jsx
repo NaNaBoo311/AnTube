331e0f9871191d039870b636dcc67edca7eaca72
NaNaBoo3101 <gtacbt0@gmail.com>
2025-07-29 15:19:39 +0700

    update side bar & navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Video from "./pages/video/Video";
 
 const App = () => {
+  const [sidebar, setSidebar] = useState(true);
+
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar setSidebar={setSidebar}></Navbar>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home sidebar={sidebar} />}></Route>
         <Route path="/video/:categoryId/:videoId" element={<Video />}></Route>
       </Routes>
     </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,20 +15,55 @@ import tom from "../../assets/tom.png";
 import megan from "../../assets/megan.png";
 import cameron from "../../assets/cameron.png";
 
-const Sidebar = () => {
+const Sidebar = ({ sidebar }) => {
   return (
-    <div className="sidebar">
-      <div className="sortcut-links">
+    <div className={`sidebar ${sidebar ? "" : "small-sidebar"}`}>
+      <div className="shortcut-links">
         <div className="side-link">
           <img src={home} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
           <img src={game_icon} alt="" /> <p>Gaming</p>
-          <img src={automobiles} alt="" /> <p>Automobiles</p>
+        </div>
+        <div className="side-link">
+          <img src={automobiles} alt="" /> <p>Auto</p>
+        </div>
+        <div className="side-link">
           <img src={sports} alt="" /> <p>Sports</p>
-          <img src={entertainment} alt="" /> <p>Entertainment</p>
+        </div>
+        <div className="side-link">
+          <img src={entertainment} alt="" /> <p>Entertain</p>
+        </div>
+        <div className="side-link">
           <img src={tech} alt="" /> <p>Technology</p>
+        </div>
+        <div className="side-link">
           <img src={music} alt="" /> <p>Music</p>
+        </div>
+        <div className="side-link">
           <img src={blogs} alt="" /> <p>Blog</p>
+        </div>
+        <div className="side-link">
           <img src={news} alt="" /> <p>New</p>
+        </div>
+        <hr></hr>
+      </div>
+      <div className="subscribed-list">
+        <h3>Kênh đăng ký</h3>
+        <div className="side-link">
+          <img src={jack} alt="" /> <p>PewDiePie</p>
+        </div>
+        <div className="side-link">
+          <img src={simon} alt="" /> <p>MrBeast</p>
+        </div>
+        <div className="side-link">
+          <img src={tom} alt="" /> <p>Justin Bieber</p>
+        </div>
+        <div className="side-link">
+          <img src={megan} alt="" /> <p>JungKook</p>
+        </div>
+        <div className="side-link">
+          <img src={cameron} alt="" /> <p>PewDiePie</p>
         </div>
       </div>
     </div>
