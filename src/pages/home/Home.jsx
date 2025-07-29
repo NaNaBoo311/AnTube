eee37217cc6319682e48bac5854a33911ef941f4
NaNaBoo3101 <gtacbt0@gmail.com>
2025-07-29 16:18:25 +0700

    add feed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../components/sidebar/Sidebar";
-
+import Feed from "../../components/feed/Feed";
 const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar}></Sidebar>
+
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Video.css";
+import PlayVideo from "../../components/playvideo/PlayVideo";
 
 const Video = () => {
-  return <div></div>;
+  return (
+    <div className="play-container">
+      <PlayVideo />
+    </div>
+  );
 };
 
 export default Video;
