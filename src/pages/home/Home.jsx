f227de997d4b3b7071777926cc068aea8e4fc27c
NaNaBoo3101 <gtacbt0@gmail.com>
2025-07-29 17:32:37 +0700

    add recommded video

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,18 @@ import "./Home.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 const Home = ({ sidebar }) => {
+  const [category, setCategory] = useState(0);
+
   return (
     <>
-      <Sidebar sidebar={sidebar}></Sidebar>
+      <Sidebar
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      ></Sidebar>
 
       <div className={`container ${sidebar ? "" : "large-container"}`}>
-        <Feed />
+        <Feed category={category} />
       </div>
     </>
   );
--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Video.css";
 import PlayVideo from "../../components/playvideo/PlayVideo";
-
+import Recommended from "../../components/recommended/Recommended";
 const Video = () => {
   return (
     <div className="play-container">
       <PlayVideo />
+      <Recommended />
     </div>
   );
 };
